72fb82a42412fca6ba5d60283dfe83f9f45e898a
Kimerland <kimerland.project@gmail.com>
2026-01-12 02:41:18 +0300

    feat(fix): fix DB url, cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     credentials: true,
   });
 
